6d2f8975015e3c672b9a11bda914dad3ce88da62
Cristal Palace <rnopal698@gmail.com>
2025-12-14 10:01:39 +0700

    fix: correct CORS origin for vercel frontend

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-  "https://music-app-frontend.vercel.app",],
+  "https://music-app-frontend-blond.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
